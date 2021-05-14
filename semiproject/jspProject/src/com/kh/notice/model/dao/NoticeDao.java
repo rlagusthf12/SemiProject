12a7118284bfafa066283b9be99199d99f63bac5
hansem <rlagusthf12@gmail.com>
2021-05-14 12:35:15 +0900

    semi

---
+++ semiproject/jspProject/src/com/kh/notice/model/dao/NoticeDao.java
@@ -0,0 +1,68 @@
+package com.kh.notice.model.dao;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Properties;
+
+import com.kh.notice.model.vo.Notice;
+
+import static com.kh.common.JDBCTemplate.*;
+
+public class NoticeDao {
+	
+	private Properties prop = new Properties();
+	
+	public NoticeDao() {
+		
+		try {
+			prop.loadFromXML(new FileInputStream( NoticeDao.class.getResource("/sql/notice/notice-mapper.xml").getPath() ));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public ArrayList<Notice> selectNoticeList(Connection conn){
+		// select문 => ResultSet객체 (여러행)
+		ArrayList<Notice> list = new ArrayList<>();
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		String sql = prop.getProperty("selectNoticeList");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			rset = pstmt.executeQuery();
+			
+			while(rset.next()) {
+				list.add(new Notice(rset.getInt("notice_no"),
+									rset.getString("notice_title"),
+									rset.getString("user_id"),
+									rset.getInt("count"),
+									rset.getDate("create_date")));
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
+		
+		return list;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
